b4cfb5edf948a7155d8c688a118b16269182ca3b
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-09-29 14:14:05 +0700

    add layout html, asset to server

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get('/', (req, res) => res.send('Hello World with Express'));
-// app.get('/', (req, res) => res.render('index.html'));
+// app.get('/', (req, res) => res.send('Hello World with Express'));
+app.get('/', (req, res) => res.render('index.html'));
 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/views/index.html'));
